e63187f5d6e55ae225762fe2ddaadc0883b08684
R R <30872@iitu.edu.kz>
2023-01-22 12:45:59 +0600

    2
    new

--- My project/Assets/Scenes/C#/player.cs
+++ My project/Assets/Scenes/C#/player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed = 7f;
+    private void Update()
     {
-        
+        GetInput();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void GetInput()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.localPosition += transform.forward * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.localPosition -= transform.forward * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localPosition += -transform.right * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localPosition += transform.right * Speed * Time.deltaTime;
+        }
     }
 }
