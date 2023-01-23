0cf1788c634eb051a86c6b318302f1b0969833cf
R R <30872@iitu.edu.kz>
2023-01-23 23:08:05 +0600

    New version
    Only for Tamina

---
+++ My project/Assets/Scenes/C#/forcamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class forcamera : MonoBehaviour
+{
+    private float mouseX;
+    private float mouseY;
+
+    public float sensitivityMouse = 200f;
+
+    public Transform Player;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sensitivityMouse * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivityMouse * Time.deltaTime;
+
+        Player.Rotate(mouseX * new Vector3(0, 1, 0));
+
+        transform.Rotate(-mouseY * new Vector3(1, 0, 0));
+    }
+}
